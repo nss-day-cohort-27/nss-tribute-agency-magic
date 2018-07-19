6c3822ad5ad9f2cdca55e169c9a218d6d4cf60f3
ricky-bruner <alteredecho07@yahoo.com>
2018-07-18 23:02:15 -0500

    commented code

--- pages/career.js
+++ pages/career.js
@@ -1,21 +1,28 @@
+// Function to retrieve data from Local Storage
 function loadData(nameTag){
     let stringifiedObject = localStorage.getItem(nameTag);
     let parsedObject = JSON.parse(stringifiedObject);
     return parsedObject;
 }
 
+// Console Log for Maximum Effort
 console.log(loadData("Career"));
 
+// Set Parsed Data to a variable for less code when transporting data through functions below
 const data = loadData("Career");
+
+// Set data paths to variable for functions below
 const intro = data.intro;
 const skills = data.skills;
 const works = data.works;
 const homes = data.homes;
 const collabs = data.collabs;
 const awards = data.awards;
+
+// Reached out to DOM to grab main div for placing data into DOM
 const mainContent = document.querySelector(".flex-container");
 
-
+// Simple Function to add intro information to DOM, is VERY specific to this information
 function addIntro(info){
     mainContent.innerHTML += 
         `<div>
@@ -24,9 +31,10 @@ function addIntro(info){
             <p>${info.bio}</p>
         </div>`;
 }
-
+// Functions called with intro variable passed into it
 addIntro(intro);
 
+// Function to populate skills variable, using a loop to add in list items
 function addSkills(info){
     mainContent.innerHTML +=
         `<div>
@@ -39,27 +47,29 @@ function addSkills(info){
             `<li>${info[i]}</li>`
     }
 }
-
+// Called in the skills variable
 addSkills(skills);
 
+// Function to create a div, build album cards, and place cards into the new div
 function addAlbums(info){
     mainContent.innerHTML += 
     `<div class="album-flex">
         <h2>Albums:</h2>
-    </div>`
-    let div = document.querySelector(".album-flex");
+    </div>` // add a class to top div for targeting purpose in function and styling 
+    let div = document.querySelector(".album-flex"); // grab the new div by the class
     for(let i = 0; i < info.length; i++){
         div.innerHTML +=
             `<div>
                 <img src="${works[i].coverArt}" alt="${works[i].name} Cover">
                 <h4>${works[i].name}</h4>
                 <h4>${works[i].releaseDate}</h4>
-            </div>`
+            </div>` // use loop to add all album cards to above div
     }
 }
-
+// Called in the works variable
 addAlbums(works)
 
+// function, similar to the albums function, to build home cards instead.
 function addHomes(info){
     mainContent.innerHTML +=
         `<div class="mansion-flex">
@@ -74,24 +84,28 @@ function addHomes(info){
             </div>`
     }
 }
-
+// Called in homes variable
 addHomes(homes);
 
+// Another list-item populating function to list the collaborators, this time targeting this ul
+// Specifically because other ul's now exist in the DOM
 function addCollabs(info){
     mainContent.innerHTML +=
         `<div>
             <h2>Common Collaborators:</h2>
             <ul class="collabs-list"></ul>
         </div>`
-    let ul = document.querySelector(".collabs-list");
+    let ul = document.querySelector(".collabs-list"); // currently in love with querySelector. 
     for (let i = 0; i < info.length; i++) {
         ul.innerHTML +=
             `<li>${info[i]}</li>`
     }
 }
-
+// Called in the collabs variable
 addCollabs(collabs);
 
+// Another card building Function for awards this time, populating a slightly more complex HTML 
+// Structure for the award cards
 function addAwards(info){
     mainContent.innerHTML += 
         `<div class="awards-flex">
@@ -110,7 +124,7 @@ function addAwards(info){
             </div>`
     }
 }
-
+// Called in final data structure variable, awards
 addAwards(awards);
 
     
