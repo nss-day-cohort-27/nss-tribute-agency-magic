e819ce5abf5d4362ef62de064f400a4366e1f8f5
johnsmccoy <johnsmccoy@comcast.net>
2018-07-19 12:43:55 -0500

    data printing to DOM

--- executive.js
+++ executive.js
@@ -1,24 +1,3 @@
-const executive = {
-    collabs: ["Twista", "Jamie Foxx", "Jay Z", "Cam'ron", "Young Jeezy","Rick Ross", "Bon Iver",
-    "Big Sean", "2 Chains", "Katie Perry"],
-
-    protrait: [
-        {
-            picture:"",
-            fullname:"Kanye Omari West"
-        }],
-    
-    aliases: ["Kanye", "Yeezy", "Ye"],
-    
-    country: ["United States"]
-}
-
-function setData(nameTag, jsObject){
-    let stringifiedObject = JSON.stringify(jsObject);
-    localStorage.setItem(nameTag, stringifiedObject);
-}
-
-setData("Executive", executive);
 
 function loadData(nameTag){
     let stringifiedObject = localStorage.getItem(nameTag);
@@ -26,3 +5,47 @@ function loadData(nameTag){
     return parsedObject;
 }
 console.log(loadData("Executive"));
+
+//Set parse data to a virable for less code when transporting data through
+//functions
+const data = loadData("Executive")
+
+const placeContent = document.getElementById("place-content");
+
+placeContent.innerHTML +=
+    `<div>
+        <h2>Collaborators</h2>
+        <ul>
+            <li>${data.collabs[0]}</li>
+            <li>${data.collabs[1]}</li>
+            <li>${data.collabs[2]}</li>
+            <li>${data.collabs[3]}</li>
+            <li>${data.collabs[4]}</li>
+            <li>${data.collabs[5]}</li>
+            <li>${data.collabs[6]}</li>
+            <li>${data.collabs[7]}</li>
+            <li>${data.collabs[8]}</li>
+            <li>${data.collabs[9]}</li>
+            <li>${data.collabs[10]}</li>
+            
+        </ul>
+    </div>`
+placeContent.innerHTML +=
+    `<div>
+        <img src="${data.portrait.picture}" alt="Kanyes Portrait">
+        <h3>${data.portrait.fullname}</h3>
+    </div>`
+ placeContent.innerHTML +=
+    `<div>
+        <h2>Country of Origin</h2>
+        <p>${data.country[0]}</p>
+    </div>`
+placeContent.innerHTML +=
+    `<div>
+        <h2>Alias</h2>
+        <ul>
+            <li>${data.aliases[0]}</li>
+            <li>${data.aliases[1]}</li>
+            <li>${data.aliases[2]}</li>
+        </ul>
+    </div>`
